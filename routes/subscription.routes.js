0036088f3a74b3c16d7d983b98d8f7dd57601a50
Mikey8250 <mohitburnwal2@gmail.com>
2026-01-31 13:20:48 +0530

    routes completed

---
+++ routes/subscription.routes.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router()
+
+
+// Get all subscriptions detail
+subscriptionRouter.get("/", ((req, res)=> {
+    res.send({title: 'GET all subscription'})
+}))
+
+// get a particular subsription detail of a customer
+subscriptionRouter.get("/:id", ((req, res)=> {
+    res.send({title: 'Get subscription details'})
+}))
+
+// Create a subscripiton
+subscriptionRouter.post("/", ((req, res)=> {
+    res.send({title: 'CREATE Subsription'})
+}))
+
+// Update a subscription
+subscriptionRouter.put("/:id", ((req, res)=> {
+    res.send({title: 'UPDATE subscription'})
+}))
+
+// delete asubscription
+subscriptionRouter.delete("/:id", ((req, res)=> {
+    res.send({title: 'DELETE subscription'})
+}))
+
+// get all user subscription of a particular user
+subscriptionRouter.get("/users/:id", ((req, res)=> {
+    res.send({title: 'Get all user subscription'})
+}))
+
+// Cancel a subscription
+subscriptionRouter.get("/:id/cancel", ((req, res)=> {
+    res.send({title: 'Cancel subscription'})
+}))
+subscriptionRouter.get("/upcoming-renewals", ((req, res)=> {
+    res.send({title: 'GET upcoming renewals'})
+}))
+
+
+export default subscriptionRouter
